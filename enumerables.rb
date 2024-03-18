18dcba89d94f2b73f20584c977727b906050322e
Victor Cai <victorcai622@gmail.com>
2024-03-18 15:59:59 -0400

    added my_select

--- enumerables.rb
+++ enumerables.rb
@@ -7,4 +7,12 @@ class Array
     end
     self
   end
+
+   def my_select(&prc)
+    selected = []
+    self.my_each do |ele|
+      selected << ele if prc.call(ele)
+    end
+    selected
+   end
 end
